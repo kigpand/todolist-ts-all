69b88a099ec985b80018dbb7f31d50fe60eb7527
kigpand <kigpand@naver.com>
2021-09-23 16:17:47 +0900

    login passport 적용 session, cookie 설정

--- todolist-server/database/sql.js
+++ todolist-server/database/sql.js
@@ -1,23 +1,12 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
 
-const connection = {
+dotenv.config();
+const connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
-    password : "@lagisin0225",
+    password : process.env.DB_PASSWORD,
     database : 'todolistdb'
-};
+});
 
-module.exports = {
-    init : () =>{
-        return mysql.createConnection(connection);
-    },
-    connect: (conn) =>{
-        conn.connect((err)=>{
-            if(err) console.error('mysql connection error : ' + err);
-            else console.log('mysql is connected successfully!')
-        })
-    },
-    end : (conn)=>{
-        conn.end();
-    }
-}
+module.exports = connection;
--- todolist-server/database/user.js
+++ todolist-server/database/user.js
@@ -1,25 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
-const db_config = require('./sql');
-const conn = db_config.init();
+const conn = require('./sql');
 
-db_config.connect(conn);
+// router.post('/login', (req, res) =>{
+//     const id = req.body.id;
+//     const pw = req.body.pw;
+//     const result = conn.query(`select * from user where (user_id ="${id}" AND user_pw = "${pw}")`, (err,rows, fields) =>{
+//         if(err) console.log('query is not excuted. select fail...\n' + err);
+//         else {
+//             console.log(rows);
+//             res.json({
+//                 result : rows
+//             });
+//         }
+//     })
+// });
 
-router.post('/login', (req, res) =>{
-    console.log(req);
-    conn.query('SELECT * from User', (err,rows, fields) =>{
-        if(err) console.log('query is not excuted. select fail...\n' + err);
-        else {
-            res.json({
-                result : rows
-            });
+router.post('/login', (req, res, next) =>{
+    passport.authenticate('local',(err, user, info) =>{
+        if(info){
+            return res.status(401).send(info.reason);
         }
-    })
+
+        return req.login(user, async()=>{
+            return res.json(user);
+        })
+    })(req, res, next);
 });
 
+router.post('/logout', (req,res)=>{
+    console.log(req.user);
+    // req.logout();
+    // req.session.destroy();
+})
+
 router.post('/join', (req, res)=>{
-    console.log(req.body);
     const userInfo = req.body.userInfo;
     conn.query(`INSERT INTO user(user_id, user_pw, user_nick) values ("${userInfo.id}", "${userInfo.pw}", "${userInfo.nickName}")`, (err, rows, fields)=>{
         if(err) {
@@ -29,7 +46,7 @@ router.post('/join', (req, res)=>{
         }else{
             res.json({
                 result : true,
-            })
+            });
         }
     })
 });
